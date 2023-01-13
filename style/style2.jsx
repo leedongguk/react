fd7865718d958e30e5900c0cc879c3aa0c5e5d64
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2023-01-13 16:15:11 +0900

    Create style2.jsx

---
+++ style/style2.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+function Library(props){
+    const name = '리액트';
+
+    return (
+        <div className="react">{name}</div>
+        );
+}
+
+export default Library;
